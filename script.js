7df3fc50df261ab4ad5f04dacb34695a0def0afa
Edson Company Colalto Junior <84912338+ecolaltojunior@users.noreply.github.com>
2023-05-08 14:00:47 -0300

    Primeiro commit - teste people analytics

---
+++ script.js
@@ -0,0 +1,46 @@
+function avaliar() {
+    // recupera as respostas do questionário
+    const form = document.getElementById("questionario")
+    const opt = form.querySelectorAll("option")
+  
+    
+    let aguia = 0
+    let lobo = 0
+    let gato = 0
+    let tubarao = 0
+    
+    
+    
+    // avalia as respostas e exibe o resultado
+    const resultado = document.getElementById("resultado");
+    let i = 0
+    if (opt[i].selected && opt[i].value === "") {
+        alert("Responda as questões")
+        return false
+    }
+    for (i = 0; i < opt.length; i++) {
+       
+        if (opt[i].selected && opt[i].value === "i") {
+            aguia++;
+        }
+        if (opt[i].selected && opt[i].value === "c") {
+            gato++;
+        }
+        if (opt[i].selected && opt[i].value === "a") {
+            tubarao++;
+        }
+        if (opt[i].selected && opt[i].value === "o") {
+            lobo++;
+        }
+               
+    }
+    
+    
+            resultado.innerHTML = "<h3>Resultado</h3> <br> Águia: " +aguia*4+ "%" +"<br>Tubarão: " +tubarao*4+"% " + "<br>Gato: " +gato*4 +"%"+ "<br>Lobo: " +lobo*4 +"%" + '<br><img src="imagem1.png" alt="resultados perfil comportamental">'
+            
+            
+         
+            
+
+}
+
