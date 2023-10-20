69e9e4b96737e929df4d77e3d965f88d9a88c7a4
ecolaltojunior <edson.colalto@fatec.sp.gov.br>
2023-10-20 17:52:53 -0300

    correção utf8

--- consultar.php
+++ consultar.php
@@ -1,12 +1,13 @@
 <?php
         require('conexao.php');
+        
         // Consulta SQL
         $sql = "SELECT * FROM perfil";
         $stmt = $conexao->prepare($sql);
         $stmt->execute();
         // Exportar dados para CSV com codificação UTF-8
     if (isset($_POST['export_csv'])) {
-    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Type: text/csv; charset=utf8');
     header('Content-Disposition: attachment; filename="dados.csv"');
     
     // Abra o arquivo de saída em modo de escrita
@@ -24,7 +25,7 @@
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         // Remover tags HTML dos valores
         $row = array_map('strip_tags', $row);
-        $row = array_map('utf8_encode', $row); // Converter todos os valores para UTF-8
+        $row = array_map('utf8_decode', $row);// Converter todos os valores para UTF-8
         fputcsv($output, $row);
     }
 
